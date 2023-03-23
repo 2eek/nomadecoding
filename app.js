cf2cddc5eb7f00f1ecca901eb2a803c7c5de1f56
2eek <khwan9404@yu.ac.kr>
2023-03-23 18:09:18 +0900

    커밋 git commit -m 커밋
    git commit -m Hi
    git commit -m hi

---
+++ app.js
@@ -0,0 +1,49 @@
+// function sayHello() {
+//   console.log('Hello my name is C');
+// }
+
+// sayHello('nico');
+// sayHello('dal');
+// sayHello('lynn');
+
+// function sayHello1(a) {
+//   return a;
+//   // console.log('Hello my name is C');
+// }
+// //sayHello1();
+// alert(sayHello1('Hello!'));
+
+// function sayHello(nameOfPerson) {
+//   console.log(nameOfPerson);
+// }
+
+// sayHello('nico');
+// sayHello('dal');
+// sayHello('lynn');
+
+// function get_argument(arg) {
+//   return arg;
+// }
+// alert(get_argument(1));
+// alert(get_argument(2));
+
+// function numbering() {
+//   console.log(1);
+// }
+// numbering();
+// numbering();
+
+// numbering();
+
+// numbering();
+
+function numbering() {
+  var i = 0;
+  while (i < 10) {
+    console.log(i);
+    i += 1;
+  }
+}
+for (var i = 0; i < 1000; i++) {
+  numbering();
+}
