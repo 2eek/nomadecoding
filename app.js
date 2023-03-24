b01e02540fe0f5eddb774b79ca6a9c83fd461037
2eek <khwan9404@yu.ac.kr>
2023-03-24 17:30:31 +0900

    230324

--- app.js
+++ app.js
@@ -22,10 +22,16 @@
 // sayHello('lynn');
 
 // function get_argument(arg) {
-//   return arg;
+//   return arg * 1000;
 // }
 // alert(get_argument(1));
 // alert(get_argument(2));
+
+// function get_arguments(arg1, arg2) {
+//   return arg1 + arg2;
+// }
+// alert(get_arguments(10, 20));
+// alert(get_arguments(20, 30));
 
 // function numbering() {
 //   console.log(1);
@@ -37,13 +43,105 @@
 
 // numbering();
 
-function numbering() {
-  var i = 0;
-  while (i < 10) {
-    console.log(i);
-    i += 1;
-  }
-}
-for (var i = 0; i < 1000; i++) {
-  numbering();
-}
+// function numbering() {
+//   var i = 0;
+//   while (i < 10) {
+//     console.log(i);
+//     i += 1;
+//   }
+// }
+// for (var i = 0; i < 1000; i++) {
+//   numbering();
+// }
+
+// function numbering() {
+//   var i = 0;
+//   while (i < 10) {
+//     console.log(i);
+//     i += 1;
+//   }
+// }
+
+// function get_member1() {
+//   return 'egoing';
+// }
+
+// function get_member2() {
+//   return 'k8805';
+// }
+
+// console.log(get_member1());
+// console.log(get_member2());
+
+// function get_member3() {
+//   return 'egoing'; // 여기까지만 출력됨.
+//   return 'leezche';
+//   return 'graphittie';
+// }
+
+// console.log(get_member3());
+
+// numbering = function () {
+//   i = 0;
+//   while (i < 10) {
+//     console.log(i);
+//     i += 1;
+//   }
+// };
+// console.log(numbering());
+
+// (function () {
+//   i = 0;
+//   while (i < 10) {
+//     console.log(i);
+//     i += 1;
+//   }
+// })();
+
+// function get_arguments(arg1, arg2) {
+//   return arg1 + arg2;
+// }
+// alert(get_arguments(10, 20));
+// alert(get_arguments(20, 30));
+
+// function sayHello(a) {
+//   console.log('Hello my name is ' + a);
+// }
+
+// function sayHello(c) {
+//   console.log('Hello my name is ' + c);
+// }
+
+// sayHello('hihihi');
+// sayHello('nico');
+// sayHello('mike');
+
+// function sayHello(nameOfPerson, age) {
+//   console.log('Hello my name is ' + nameOfPerson + " and I'm", age);
+// }
+
+// sayHello('nico', 10);
+// sayHello('dal', 23);
+// sayHello('lynn', 21);
+
+// const player = {
+//   name: 'nico',
+//   sayHello: function (otherPersonsName) {
+//     console.log('hello ' + otherPersonsName + ' nice to meet you');
+//   },
+// };
+
+// console.log(player.name);
+// player.sayHello('lynn');
+
+// function plus(a, b) {
+//   console.log(a + b);
+// }
+
+// plus(5, 5);
+
+const toBuy = ['potato', 'tomato', 'pizza'];
+toBuy[2] = 'water';
+console.log(toBuy);
+toBuy.push('meet');
+console.log(toBuy);
